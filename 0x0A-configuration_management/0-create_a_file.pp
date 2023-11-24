b0a67178e41c3c4ba504d720be5a7b8941b68be3
Sadson <sadsonmkumira@yahoo.com>
2023-11-24 12:06:47 +0000

    Adding the file 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# creates a file in /tmp using puppet
+file { '/tmp/school':
+  ensure  => present,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
