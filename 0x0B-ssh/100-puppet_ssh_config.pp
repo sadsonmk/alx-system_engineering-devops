451f752c39c990edbc2ee812f95d862abeaffa8d
Sadson <sadsonmkumira@yahoo.com>
2023-11-25 13:15:16 +0000

    Adding files

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,13 +5,13 @@ file { '/etc/ssh/ssh_config':
   ensure  => present,
 }
 
-file_line { 'configure private key':
+file_line { 'Declare identity file':
   path    => '/etc/ssh/ssh_config',
   line    => 'IdentityFile ~/.ssh/school',
   match   => '^#IdentityFile',
 }
 
-file_line { 'Ignore password':
+file_line { 'Turn off passwd auth':
   path    => '/etc/ssh/ssh_config',
   line    => 'PasswordAuthentication no',
   match   => '^#PasswordAuthentication',
