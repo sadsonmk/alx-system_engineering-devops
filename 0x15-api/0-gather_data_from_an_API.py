096b295b3cb82eba0f235dda494497cffd632374
Sadson <sadsonmkumira@yahoo.com>
2024-01-02 07:34:55 +0000

    Adding the file readme.md and 0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""using a REST API, for a given employee ID, returns
+    information about his/her TODO list progress.
+"""
+
+import requests
+import sys
+
+employee_id = int(sys.argv[1])
+url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+url_users = "https://jsonplaceholder.typicode.com/users"
+
+data = requests.get(url)
+usr = requests.get(url_users)
+
+TOTAL_NUMBER_OF_TASKS = len(data.json())
+done_tasks = [x for x in data.json() if x.get('completed') is True]
+NUMBER_OF_DONE_TASKS = len(done_tasks)
+
+for user in usr.json():
+    user_id = int(user.get('id'))
+    if user_id == employee_id:
+        EMPLOYEE_NAME = user.get('name')
+print(f"Employee {EMPLOYEE_NAME} is done with \
+tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+
+for info in done_tasks:
+    my_tasks = info.get('title')
+    print(f"\t {my_tasks}")
