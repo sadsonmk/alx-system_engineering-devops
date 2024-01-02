09859e4109aa55149e47925f6bbbd08c9ec2dabb
Sadson <sadsonmkumira@yahoo.com>
2024-01-02 09:59:37 +0000

    Adding the file 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,29 +7,30 @@ import csv
 import requests
 import sys
 
-emp_id = int(sys.argv[1])
-print('retrieving data')
-url = f"https://jsonplaceholder.typicode.com/users/{emp_id}/todos"
-user_url = "https://jsonplaceholder.typicode.com/users"
+if __name__ == '__main__':
+    emp_id = int(sys.argv[1])
+    print('retrieving data')
+    url = f"https://jsonplaceholder.typicode.com/users/{emp_id}/todos"
+    user_url = "https://jsonplaceholder.typicode.com/users"
 
-data = requests.get(url)
-usr_data = requests.get(user_url)
-info = data.json()
-usr_info = usr_data.json()
+    data = requests.get(url)
+    usr_data = requests.get(user_url)
+    info = data.json()
+    usr_info = usr_data.json()
 
-for user in usr_info:
-    user_id = int(user.get('id'))
-    if user_id == emp_id:
-        USERNAME = user.get('username')
+    for user in usr_info:
+        user_id = int(user.get('id'))
+        if user_id == emp_id:
+            USERNAME = user.get('username')
 
-result = []
-for user in info:
-    USER_ID = user.get('userId')
-    TASK_COMPLETED_STATUS = user.get('completed')
-    TASK_TITLE = user.get('title')
-    ln = list(f'{USER_ID}, {USERNAME}, {TASK_COMPLETED_STATUS}, {TASK_TITLE}')
-    result.append(ln)
+    result = []
+    for user in info:
+        USER_ID = user.get('userId')
+        TASK_COMPLETED_STATUS = user.get('completed')
+        TASK_TITLE = user.get('title')
+        ln = f'{USER_ID},{USERNAME},{TASK_COMPLETED_STATUS},{TASK_TITLE}'
+        result.append(ln.split(','))
 
-with open(f'{USER_ID}.csv', 'w',) as csfile:
-    write = csv.writer(csfile)
-    write.writerows(result)
+    with open(f'{USER_ID}.csv', 'w',) as csfile:
+        write = csv.writer(csfile)
+        write.writerows(result)
