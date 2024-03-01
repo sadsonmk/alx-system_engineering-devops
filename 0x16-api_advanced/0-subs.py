c530affce1b85d9ff0b79fd347067b8bd805c0f7
Sadson <sadsonmkumira@yahoo.com>
2024-03-01 10:19:52 -0800

    Adding the file 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,12 +13,12 @@ def number_of_subscribers(subreddit):
 
     return: number of subscribers or 0 if not valid
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about"
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()
-        return (data['subscribers'])
+        data = response.json().get('data')
+        return (data.get('subscribers'))
     else:
         return (0)
